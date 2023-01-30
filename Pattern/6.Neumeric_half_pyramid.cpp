75cd2526cf597da9f12b322e80e90bae5cc6ff78
sarasijbasu <67532985+sarasijbasu@users.noreply.github.com>
2023-01-30 16:05:47 +0530

    Update 6.Neumeric_half_pyramid.cpp

--- Pattern/6.Neumeric_half_pyramid.cpp
+++ Pattern/6.Neumeric_half_pyramid.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 using namespace std;
 int main()
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+// 123456
+// 1234567
+// 12345678
+// 123456789
+// Output should be like this for 9 rows.
 {
     int no_of_rows;
     cout << "Enter the number of rows : ";
     cin >> no_of_rows;
     for (int row = 0; row < no_of_rows; row++)
     {
-        /* code */
         for (int col = 0; col < row + 1; col++)
         {
-            /* code */
             cout << col + 1;
         }
         cout << endl;
