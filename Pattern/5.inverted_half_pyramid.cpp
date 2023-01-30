8d5012e6b48ea2cf4077770e51f59b52b9140072
sarasijbasu <67532985+sarasijbasu@users.noreply.github.com>
2023-01-30 13:33:05 +0530

    Add files via upload

---
+++ Pattern/5.inverted_half_pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int no_of_rows;
+    cout << "Enter the number of rows : ";
+    cin >> no_of_rows;
+    for (int row = 0; row < no_of_rows; row++)
+    {
+        /* code */
+        for (int col = 0; col < no_of_rows - row; col++)
+        {
+            /* code */
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
