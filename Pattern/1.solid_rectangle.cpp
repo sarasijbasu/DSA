25fdc88742a3bf5f1daa88eaee77fac4e491aabd
sarasijbasu <67532985+sarasijbasu@users.noreply.github.com>
2023-01-30 15:58:47 +0530

    Update 1.solid_rectangle.cpp

--- Pattern/1.solid_rectangle.cpp
+++ Pattern/1.solid_rectangle.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
+// ****
+// ****
+// ****
+// output should be like this for 3 rows and 4 columns
 
-int main() {
-  // Rectangular
-  int row_no, col_no;
-  cout << "Enter Number of Rows and Number of Columns : " << endl;
-  cin >> row_no >> col_no;
-  for (int row = 0; row < row_no; row++) {
-    for (int col = 0; col < col_no; col++) {
-      cout << "*";
+int main()
+{
+    // Rectangular
+    int row_no, col_no;
+    cout << "Enter Number of Rows and Number of Columns : " << endl;
+    cin >> row_no >> col_no;
+    for (int row = 0; row < row_no; row++)
+    {
+        for (int col = 0; col < col_no; col++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }
