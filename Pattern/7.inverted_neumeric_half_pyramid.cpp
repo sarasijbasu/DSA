c58cb16ab49ad474620c3d949088fa8052d554ac
sarasijbasu <67532985+sarasijbasu@users.noreply.github.com>
2023-01-30 16:08:25 +0530

    Update 7.inverted_neumeric_half_pyramid.cpp

--- Pattern/7.inverted_neumeric_half_pyramid.cpp
+++ Pattern/7.inverted_neumeric_half_pyramid.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 using namespace std;
-int main()
 
-// 123456789
-// 12345678
-// 1234567
-// 123456
-// 12345
-// 1234
-// 123
-// 12
-// 1
+//         * 
+//        * *
+//       * * *
+//      * * * *
+//     * * * * *
+//    * * * * * *
+//   * * * * * * *
+//  * * * * * * * * 
+// * * * * * * * * *
 // Output should be like this for 9 rows
-
+int main()
 {
     int no_of_rows;
     cout << "Enter the number of rows : ";
     cin >> no_of_rows;
     for (int row = 0; row < no_of_rows; row++)
     {
-        /* code */
-        for (int col = 0; col < no_of_rows - row; col++)
+        // code for printing space
+        for (int i = 0; i < no_of_rows - 1 - row; i++)
         {
-            /* code */
-            cout << col + 1;
+            cout << " ";
+        }
+        //code for printing star
+        for (int j = 0; j < row + 1; j++)
+        {
+            cout << "* ";
         }
         cout << endl;
     }
+
+    return 0;
 }
