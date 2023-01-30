6c38e057810956d4bc283869749055fa5baf9814
sarasijbasu <67532985+sarasijbasu@users.noreply.github.com>
2023-01-30 15:38:41 +0530

    Add files via upload

---
+++ Pattern/8.full_pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int no_of_rows;
+    cout << "Enter the number of rows : ";
+    cin >> no_of_rows;
+    for (int row = 0; row < no_of_rows; row++)
+    {
+        // code for printing space
+        for (int i = 0; i < no_of_rows - 1 - row; i++)
+        {
+            /* code */
+            cout << " ";
+        }
+        for (int j = 0; j < row + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
