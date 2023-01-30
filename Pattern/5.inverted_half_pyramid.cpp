eed265055ffdcbda245395e727763c91187447af
sarasijbasu <67532985+sarasijbasu@users.noreply.github.com>
2023-01-30 16:04:02 +0530

    Update 5.inverted_half_pyramid.cpp

--- Pattern/5.inverted_half_pyramid.cpp
+++ Pattern/5.inverted_half_pyramid.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main()
+
+// *****
+// ****
+// ***
+// **
+// *
+// Output should be like this for 5 rows
+
 {
     int no_of_rows;
     cout << "Enter the number of rows : ";
